3d5b9e70a85e42818f9ef8d5033795a003a7a843
Kawaiiskii <antonzhadnov@gmail.com>
2023-03-30 00:30:27 +0300

    complete Team pojo

--- src/main/java/entity/Team.java
+++ src/main/java/entity/Team.java
@@ -1,4 +1,71 @@
 package entity;
 
+import java.util.Objects;
+
 public class Team {
+    private int id;
+    private String name;
+    private String courseName;
+
+    public Team() {
+    }
+
+    public Team(int id, String name, String courseName) {
+        this.id = id;
+        this.name = name;
+        this.courseName = courseName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Team team = (Team) o;
+
+        if (id != team.id) return false;
+        if (!Objects.equals(name, team.name)) return false;
+        return Objects.equals(courseName, team.courseName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (courseName != null ? courseName.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", courseName='" + courseName + '\'' +
+                '}';
+    }
 }
